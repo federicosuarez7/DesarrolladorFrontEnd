05e6c189c8a30deb3ee0d600e678948340a4a2af
federicosuarez7 <federico.saboretto@gmail.com>
2024-07-28 21:35:09 -0300

    Vemos funciones declaradas y expresadas (JS)

---
+++ FrontEnd/js/funciones.js
@@ -0,0 +1,12 @@
+// Funciones declaradas
+function funcDeclarada() {
+    console.log("Hola soy una funcion declarada ");
+}
+funcDeclarada();
+
+// Funciones expresadas
+const funcExpresada = function (){
+    console.log("Hola soy una funcion expresada");
+}
+
+funcExpresada();
