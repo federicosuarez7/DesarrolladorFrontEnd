bf4107977889279ebaf5a01a6dd5e132f425a188
federicosuarez7 <federico.saboretto@gmail.com>
2024-07-31 14:24:27 -0300

    Comienzo con arrow functions (JS)

---
+++ FrontEnd/js/arrowFunctions.js
@@ -0,0 +1,12 @@
+// ArrowFunction sin parametros
+const saludo = ()=>{
+    console.log("Hola");
+}
+
+// ArrowFunction con parametros
+const saludarNombre = (nombre) =>{
+    console.log("Hola como estas "+nombre);
+}
+
+saludo();
+saludarNombre("Federico");
